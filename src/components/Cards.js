d29472344e9573ebf1a300ae637373c2f54f0c21
yitingLIU211 <yliu0253@student.monash.edu>
2021-04-06 17:53:01 +1000

    homepage and impact summary page css change

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -28,21 +28,25 @@ export class Cards extends Component {
             <ul className="cards-items">
               <CardItem
                 src="images/impact_enviorment.jpg"
-                text="Littering can have major influence on scenic beauty of the environment. It can contaminate soil and pollute water. Burning of litter can increase emission of harmful gases leading to air pollution.
-                "
+                // text="
+                // Littering can have major influence on scenic beauty of the environment. It can contaminate soil and pollute water. Burning of litter can increase emission of harmful gases leading to air pollution.
+                // "
+                text=""
                 label="Impact on Environment "
                 path="/impact_enviorment"
               />
               <CardItem
                 src="images/figure2.png"
-                text="Litter can have an adverse impact on health of people as well as animals. Inefficient litter disposal can create unhygienic living conditions, block drainage systems and spread diseases. Litter can be injurious to Australian wildlife due to ingestion or entanglement that can lead to suffocation, drowning and maiming."
+                // text="Litter can have an adverse impact on health of people as well as animals. Inefficient litter disposal can create unhygienic living conditions, block drainage systems and spread diseases. Litter can be injurious to Australian wildlife due to ingestion or entanglement that can lead to suffocation, drowning and maiming."
+                text=""
                 label="Impact on Human & Animals"
                 path="/impact_human_animal"
               />
               <CardItem
                 src="images/figure3.jpg"
-                text="The benefits of garbage sorting are obvious. Garbage classified collection can reduce the amount of garbage disposal and treatment equipment, reduce treatment costs, and reduce the consumption of land resources. It has social, economic, and ecological benefits.
-                "
+                // text="The benefits of garbage sorting are obvious. Garbage classified collection can reduce the amount of garbage disposal and treatment equipment, reduce treatment costs, and reduce the consumption of land resources. It has social, economic, and ecological benefits.
+                // "
+                text=""
                 label="Litter Classification"
                 path="/impact_classification"
               />
